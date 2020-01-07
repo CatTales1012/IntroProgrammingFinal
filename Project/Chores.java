45bfdd2886bf6a3343c7b1dcdbdc50bedbaa4b6b
404kevnotfound <student@HSiMac4-61.local>
2020-01-07 12:57:37 -0700

    Updated Chores
    now generates at least 2 chores each person.

--- Project/Chores.java
+++ Project/Chores.java
@@ -2,15 +2,16 @@
 Author: Cat Smith
 Assignment: Final Project- a generic chore assignment generator.
 Started: Dec 19
-Finished: **After Winter Break**
+Finished: **After Winter Break** January 
 */
 
 import java.util.Scanner;
 class Chores {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String[] bigChores = {"Kitchen", "Living Room", "Bathroom"};
-		String[] smallChores = {"Sweep / Mop", "Vacuum / Dust", "Mirrors / Windows"};
+		
+		String[] bigChores = {"Kitchen", "Living Room", "Bathroom", "Front Yard / Porch"};
+		String[] smallChores = {"Sweep / Mop", "Vacuum / Dust", "Mirrors / Windows", "Water Plants"};
 		
 		System.out.print("Please enter between 2-6 for number of people: ");
 		int numOfPeople = input.nextInt();
@@ -22,6 +23,26 @@ class Chores {
 			names[i] = input.nextLine();
 		}
 		
-		//page 255 has a random pull for generator- check here distracto
+		
+		//use a string to contain an already used random number to avoid repeats. 
+		String bChoose = "";
+		String sChoose = "";
+		
+		for(int i = 1; i < names.length; i++){
+			int big = (int)(Math.random() * bigChores.length);
+			while (bChoose.contains(Integer.toString(big))) {
+				big = (int)(Math.random() * bigChores.length);
+			}
+			bChoose += big + " ";
+			
+			int small = (int)(Math.random() * smallChores.length);
+			while (sChoose.contains(Integer.toString(small))) {
+				small = (int)(Math.random() * smallChores.length);
+			}
+			sChoose += small + " ";
+
+			System.out.println(names[i] + " - " + bigChores[big] + ", " + smallChores[small] );
+		}
+		
 	}
 }
